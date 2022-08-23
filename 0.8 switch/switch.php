3dbf47899bd99ed05714b17ec04058b797cbed31
islam_deve <islamalaham90k50@gmail.com>
2022-08-23 06:57:13 +0300

    switch1

---
+++ 0.8 switch/switch.php
@@ -0,0 +1,16 @@
+<?php
+$cars="caya";
+switch ($cars) {
+  case "caya":
+    echo "Your cars is remshy!";
+    break;
+  case "scoia":
+    echo "Your cars is scoia!";
+    break;
+  case "mersits":
+    echo "Your cars is mersits!";
+    break;
+  default:
+    echo "Your cars is neither remshy, scoia, nor mersits!";
+}
+?>
