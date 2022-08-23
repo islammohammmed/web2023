957b8dca314615ac62572dca74a44376ad320580
islam_deve <islamalaham90k50@gmail.com>
2022-08-23 06:58:28 +0300

    foreach

---
+++ foreach/foreach.php
@@ -0,0 +1,11 @@
+<?php
+$colors=array("red","blue","green");
+foreach($colors as $value){
+    echo "$value <br>";
+}
+
+$age=array("poter"=>"35","rami"=>"43","ali"=>"40");
+foreach($age as $x=>$val){
+    echo "$val ."<br>";
+}
+?>
