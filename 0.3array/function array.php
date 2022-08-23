dadec3f1f825bc1205c1573acf4bc94de28115e0
islam_deve <islamalaham90k50@gmail.com>
2022-08-23 06:56:16 +0300

    function array

---
+++ 0.3array/function array.php
@@ -0,0 +1,14 @@
+<?php
+$cars=array("red","blue","black");
+echo sort($cars);
+echo"<br>";
+$name=array("Ali","Sami","Alo");
+echo rsort($cars);
+echo"<br>";
+$number=array(1,2,4,5,7);
+echo asort($cars);
+echo"<br>";
+$colors=array("yellow","green","red");
+echo ksort($cars);
+
+?>
