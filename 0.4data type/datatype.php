e8d4d1bdeddda0a68dbb3749a664b944a6c15eed
islam_deve <islamalaham90k50@gmail.com>
2022-08-23 06:56:43 +0300

    datatype.php

---
+++ 0.4data type/datatype.php
@@ -0,0 +1,14 @@
+<?php 
+ $float_var=1.5;
+ $int_var=intval($float_var);
+ echo $int_var;
+ $str_var=strval($float_var);
+
+ var_dump($int_var);
+ var_dump($str_var);
+
+$str="welcome";
+$float=floatval($str);
+var_dump($float);
+
+?>
