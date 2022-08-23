0703d15690363b46fa1e0f059344402202abfd60
islam_deve <islamalaham90k50@gmail.com>
2022-08-23 06:55:05 +0300

    variablesample

---
+++ 0.2 variabble/sample.php
@@ -0,0 +1,15 @@
+<?php
+//case-sensitive
+$color="red";
+echo"My car is".$Color."<br>";
+echo"My boat is".$color."<br>";
+$text="hello world";
+$x=5;
+$y=10.9;
+echo $text;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+echo "<br>";
+?>
